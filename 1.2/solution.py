bf70e6155ef358c48e8d07f097f0c89c87dfc07a
THelsby <root@localhost.localdomain>
2023-12-12 20:43:12 +0000

    day 1 solutions

--- 1.2/solution.py
+++ 1.2/solution.py
@@ -1,69 +1,58 @@
+import re
+import time
+
 print('RUNNING')
-file = open("input.txt", "r")
+
+numbers_dict = {
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9,
+}
+
+def check_string(sub_string):
+    for key in numbers_dict:
+        if key in sub_string:
+            return numbers_dict[key]
+    return None
 
 def find_first_number(line):
-    for i in range(len(line)):
-        if line[i].isnumeric():
-            return line[i]
+    for i, character in enumerate(line):
+        if character.isnumeric():
+            return character
+        elif i < len(line) - 1 and line[i + 1].isnumeric():
+            return line[i + 1]
         else:
-            if 'one' in line[i:i+5]:
-                return 1
-            if 'two' in line[i:i+5]:
-                return 2
-            if 'three' in line[i:i+5]:
-                return 3
-            if 'four' in line[i:i+5]:
-                return 4
-            if 'five' in line[i:i+5]:
-                return 5
-            if 'six' in line[i:i+5]:
-                return 6
-            if 'seven' in line[i:i+5]:
-                return 7
-            if 'eight' in line[i:i+5]:
-                return 8
-            if 'nine' in line[i:i+5]:
-                return 9
-                        
-            
+            found = check_string(line[i:i + 5])
+            if found:
+                return found
 
 def find_last_number(line):
-    for i in range(len(line)):
-        i = i + 1
-        if line[len(line)-i].isnumeric():
-            return line[len(line)-i]
+    for i in range(len(line) - 1, -1, -1):
+        if line[i].isnumeric():
+            return line[i]
+        elif i > 0 and line[i - 1].isnumeric():
+            return line[i - 1]
         else:
-            point_1 = len(line)-i+1
-            point_2 = len(line)-i-5
-            if 'one' in line[point_2:point_1]:
-                return 1
-            if 'two' in line[point_2:point_1]:
-                return 2
-            if 'three' in line[point_2:point_1]:
-                return 3
-            if 'four' in line[point_2:point_1]:
-                return 4
-            if 'five' in line[point_2:point_1]:
-                return 5
-            if 'six' in line[point_2:point_1]:
-                return 6
-            if 'seven' in line[point_2:point_1]:
-                return 7
-            if 'eight' in line[point_2:point_1]:
-                return 8
-            if 'nine' in line[point_2:point_1]:
-                return 9
+            found = check_string(line[max(i - 4, 0):i + 1])
+            if found:
+                return found
 
-def find_first_and_last_numbers(file):
-    list_of_numbers = []
-    for line in file.readlines():
-        print(line)
-        print(f"{find_first_number(line)}{find_last_number(line)}")
-        list_of_numbers.append(f"{find_first_number(line)}{find_last_number(line)}")
-
-    total = 0
-    for number in list_of_numbers:
-        total = total + int(number)
+def find_first_and_last_numbers(file_path):
+    with open(file_path, "r") as file:
+        lines = file.readlines()
+        
+    numbers = [f"{find_first_number(line.rstrip())}{find_last_number(line.rstrip())}" for line in lines]
+    total = sum(int(number) for number in numbers if number.isdigit())
     print(total)
 
-find_first_and_last_numbers(file)
+start_time = time.time()
+find_first_and_last_numbers("input.txt")
+end_time = time.time()
+duration = end_time - start_time
+print("Duration in seconds:", duration)
---
+++ 1.2/zac_test.py
@@ -0,0 +1,56 @@
+import re
+import time
+
+total_of_numbers = 0
+numbers_words_dict = {'one':1,
+                      'two':2,
+                      'three':3,
+                      'four':4,
+                      'five':5,
+                      'six':6,
+                      'seven':7,
+                      'eight':8,
+                      'nine':9,
+                      'enin':9,
+                      'thgie':8,
+                      'neves':7,
+                      'xis':6,
+                      'evif':5,
+                      'ruof':4,
+                      'eerht':3,
+                      'owt':2,
+                      'eno':1
+                      }
+
+def find_first_number(text,first=True):
+    last_pattern = re.compile(r'(\d|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin)',re.IGNORECASE)
+    pattern = re.compile(r'(\d|one|two|three|four|five|six|seven|eight|nine)',re.IGNORECASE)
+    if first:
+        match = pattern.search(text)
+    else:
+        match = last_pattern.search(text)
+    if len(match.group()) > 1:
+        return numbers_words_dict[match.group()]
+    if match:
+        return match.group()
+    else:
+        return 0
+
+def process_line(line):
+    global total_of_numbers
+    total_of_numbers += int(f"{find_first_number(line)}{find_first_number(line[::-1],False)}")
+
+def process_file(file_path):
+    with open(file_path, 'r') as file:
+        for line in file:
+            line = line.rstrip()
+            process_line(line)
+
+# Example usage
+file_path = 'input.txt'
+start_time = time.time()
+process_file(file_path)
+print(total_of_numbers)
+end_time = time.time()
+duration = end_time - start_time
+print("Duration in seconds:", duration)
